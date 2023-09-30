d9a8ab7d36c752dffe9b43f57f1c103cd4b53cac
ikramulhasan1 <hasanikramul926@gmail.com>
2023-10-01 03:13:20 +0600

    Update

---
+++ resources/views/frontend/layouts/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="bg-dark text-center text-lg-start">
+    <div class="text-center p-3 text-light" style="background-color: rgba(0, 0, 0, 0.2);">
+        © 2020 Copyright:
+        <a class="text-light" href="">Ikramul</a>
+    </div>
+
+</footer>
---
+++ resources/views/frontend/layouts/partials/navbar2.blade.php
@@ -0,0 +1,36 @@
+<nav class="navbar sticky-top navbar-expand-lg bg-dark border-bottom border-body">
+    <div class="container ">
+        <a class="navbar-brand  text-white" href="#">
+            <img src="./asset/image/Ecommerce.png" alt="Logo" width="85" height="35"
+                class="d-inline-block align-text-top ">
+
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon bg-white"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active text-white" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#">Product</a>
+                </li>
+
+
+            </ul>
+            <div class="d-flex">
+                <form class="d-flex col-sm-6" role="search">
+                    <input class="form-control p-1 rounded-0 border-0 me-2" type="search" placeholder="Search"
+                        aria-label="Search">
+                    <button class="btn btn-outline-light py-1 px-2 rounded-0 border-1" type="submit">Search</button>
+                </form>
+                <div class="d-flex col-sm-6 gap-2">
+                    <a href="#" class="btn btn-primary btn-sm border-0 rounded-0 ms-2">Login</a>
+                    <a href="#" class="btn btn-primary btn-sm border-0 rounded-0 ">Register</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
